3c001e3f13d0209f73db02edcc3163cbb3e52d28
Dylan <dyl.griffith@gmail.com>
2014-01-24 14:06:46 -0600

    add back the vimrc and add some vim plugins

--- link_all.sh
+++ link_all.sh
@@ -1,9 +1,14 @@
 PWD=`pwd`
 if [ ! -d "$HOME/.vim" ]; then
     mkdir ~/.vim
+fi
+if [ ! -d "$HOME/.vim/bundle" ]; then
     mkdir ~/.vim/bundle
 fi
-ln -s $PWD/vimfiles/vimrc ~/.vimrc
+if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+ln -s $PWD/configs/vimrc ~/.vimrc
 ln -s $PWD/configs/gitconfig ~/.gitconfig
 ln -s $PWD/configs/bashrc ~/.bashrc
 ln -s $PWD/configs/gitignore_global ~/.gitignore_global
@@ -13,4 +18,3 @@ ln -s $PWD/configs/rvmrc ~/.rvmrc
 ln -s $PWD/configs/tmux.conf ~/.tmux.conf
 ln -s $PWD/configs/zshrc ~/.zshrc
 ln -s $PWD/tmux_clip.sh ~/.tmux_clip.sh
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
