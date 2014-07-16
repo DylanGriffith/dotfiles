853e9f67b377a814c647d7793e431168c749637e
Dylan <dyl.griffith@gmail.com>
2014-07-16 20:12:30 +1000

    safer linking allow spaces, add todo.txt config

--- link_all.sh
+++ link_all.sh
@@ -8,13 +8,14 @@ fi
 if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
-ln -s $PWD/configs/vimrc ~/.vimrc
-ln -s $PWD/configs/gitconfig ~/.gitconfig
-ln -s $PWD/configs/bashrc ~/.bashrc
-ln -s $PWD/configs/gitignore_global ~/.gitignore_global
-ln -s $PWD/configs/pryrc ~/.pryrc
-ln -s $PWD/configs/rspec ~/.rspec
-ln -s $PWD/configs/rvmrc ~/.rvmrc
-ln -s $PWD/configs/tmux.conf ~/.tmux.conf
-ln -s $PWD/configs/zshrc ~/.zshrc
-ln -s $PWD/tmux_clip.sh ~/.tmux_clip.sh
+ln -s "$PWD/configs/vimrc" ~/.vimrc
+ln -s "$PWD/configs/gitconfig" ~/.gitconfig
+ln -s "$PWD/configs/bashrc" ~/.bashrc
+ln -s "$PWD/configs/gitignore_global" ~/.gitignore_global
+ln -s "$PWD/configs/pryrc" ~/.pryrc
+ln -s "$PWD/configs/rspec" ~/.rspec
+ln -s "$PWD/configs/rvmrc" ~/.rvmrc
+ln -s "$PWD/configs/tmux.conf" ~/.tmux.conf
+ln -s "$PWD/configs/zshrc" ~/.zshrc
+ln -s "$PWD/configs/todo.cfg" ~/.todo.cfg
+ln -s "$PWD/tmux_clip.s" ~/.tmux_clip.sh
